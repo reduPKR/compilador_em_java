e6d669fde201b98d821e18b5cf72739e93731422
reduPKR <REDUA@DESKTOP-48REONQ>
2020-02-28 09:41:58 -0300

    Salvando, erros nas letras e =

--- src/utils/CodeAreaClass.java
+++ src/utils/CodeAreaClass.java
@@ -1,47 +1,39 @@
 package utils;
  
-import java.time.Duration;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import javafx.application.Platform;
-import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.StackPane;
-
-import org.fxmisc.flowless.VirtualizedScrollPane;
 import org.fxmisc.richtext.CodeArea;
 import org.fxmisc.richtext.LineNumberFactory;
 import org.fxmisc.richtext.model.StyleSpans;
 import org.fxmisc.richtext.model.StyleSpansBuilder;
-import org.reactfx.Subscription;
 
 public class CodeAreaClass {
-    private static final String[] KEYWORDS = new String[]{"boolean", "char", "double", "else", "false", "for", "if", "int", "mentira", "null", "nulo", "NULL", "program", "string", "true", "verdade", "while"};
-    //private static final String[] SIMBOLOS = new String[]{"a, b, c, d, e, f"};
+    private static final String[] TIPOS = new String[]{"int", "double", "string", "bool"};
+    private static final String[] BLOCOS = new String[]{"if", "else", "while", "loop"};
     
-    //private static final String SIMBOLOS_PATTERN = "\\b(" + String.join("|", SIMBOLOS) + ")\\b";
-    private static final String KEYWORD_PATTERN = "\\b(" + String.join("|", KEYWORDS) + ")\\b";
-    private static final String LETTERS_PATTERN = "[A-Za-z#`$._]+";
-    private static final String SIMBOLOS_PATTERN = "\\{|\\}|\\[|\\]|\\(|\\)|\\%|\\;|\\<|\\>|\\*|\\=|\\?|\\:|\\||\\&|\\+|\\-|\\/|\\~|\\!|\\^";
-    private static final String NUMBERS_PATTERN = "[0-9]+";
-    private static final String CHAR_PATTERN = "\'[\\S+\\s]{1}\'";
-    private static final String STRING_PATTERN = "\\\"([^\\\"\\\\\\\\]|\\\\\\\\.)*\\\"";
-    private static final String COMENT_PATTERN = "\\@([^\\@\\\\\\\\]|\\\\\\\\.)*\\@";
-    private static final String RESTO_PATTERN = "\\S+";
+    private static final String PROGRAMA_PATTERN = "\\<\\?|\\?\\>";
+    private static final String TIPO_PATTERN = "\\b(" + String.join("|", TIPOS) + ")\\b";
+    private static final String BLOCO_PATTERN = "\\b(" + String.join("|", BLOCOS) + ")\\b";
+    private static final String LIMITE_PATTERN = "\\(|\\)|\\{|\\}";
+    private static final String ATRIBUICAO_PATTERN = "=";/*"\\=|"+LETRAS_PATTERN+"\\=";*/
+    private static final String ARITMETICO_PATTERN = "\\+|\\-|\\*|\\/";
+    private static final String COMPARACAO_PATTERN = "\\==|\\!=|\\>|\\<|\\>=|\\<=";
+    private static final String LOGICO_PATTERN = "\\&|\\||";
+    private static final String LETRA_PATTERN = "a";
     
-    private static final Pattern PATTERN = Pattern.compile(
-            "(?<KEYWORD>" + KEYWORD_PATTERN + ")" +
-            "|(?<LETTERS>" + LETTERS_PATTERN + ")" +
-            "|(?<SIMBOLOS>" + SIMBOLOS_PATTERN + ")" +
-            "|(?<NUMBERS>" + NUMBERS_PATTERN + ")" +
-            "|(?<CHAR>" + CHAR_PATTERN + ")" +
-            "|(?<STRING>" + STRING_PATTERN + ")" +
-            "|(?<COMENT>" + COMENT_PATTERN + ")" +
-            "|(?<RESTO>" + RESTO_PATTERN + ")"
+    private static final Pattern PATTERN = Pattern.compile(            
+            "(?<PROGRAMA>" + PROGRAMA_PATTERN + ")" +
+            "|(?<TIPO>" + TIPO_PATTERN + ")" +
+            "|(?<BLOCO>" + BLOCO_PATTERN + ")" +
+            "|(?<LIMITE>" + LIMITE_PATTERN + ")" +
+            "|(?<ATRIBUICAO>" + ATRIBUICAO_PATTERN + ")" +
+            "|(?<ARITMETICO>" + ARITMETICO_PATTERN + ")" +
+            "|(?<COMPARACAO>" + COMPARACAO_PATTERN + ")" +
+            "|(?<LOGICO>" + LOGICO_PATTERN + ")" +
+            "|(?<LETRA>" + LETRA_PATTERN + ")"
     );
     
     public static CodeArea IniciarCodeArea(CodeArea ca){
@@ -67,13 +59,16 @@ public class CodeAreaClass {
         while (matcher.find()) 
         {
             String styleClass = 
-                matcher.group("KEYWORD") != null ? "keyword": 
-                matcher.group("LETTERS") != null ? "letters" :  
-                matcher.group("SIMBOLOS") != null ? "simbolo" : 
-                matcher.group("NUMBERS") != null ? "numeros" :
-                matcher.group("CHAR") != null ? "char" :
-                matcher.group("STRING") != null ? "string" :
-                matcher.group("COMENT") != null ? "coment" : null;
+                matcher.group("PROGRAMA") != null ? "programa":
+                matcher.group("TIPO") != null ? "tipo": 
+                matcher.group("BLOCO") != null ? "bloco": 
+                matcher.group("LIMITE") != null ? "limite":   
+                matcher.group("ATRIBUICAO") != null ? "atribuicao":
+                matcher.group("ARITMETICO") != null ? "aritmetico": 
+                matcher.group("COMPARACAO") != null ? "compracao": 
+                matcher.group("LETRA") != null ? "letra":
+                matcher.group("LOGICO") != null ? "logico":
+                null;
             
             assert styleClass != null;
             spansBuilder.add(Collections.emptyList(), matcher.start() - lastKwEnd);
