9621b42c1a7453703b640e6f6ef6bb5c5aaa388d
reduPKR <REDUA@DESKTOP-48REONQ>
2020-05-20 22:53:55 -0300

    Mudancas que nao estragaram meu projeto

--- src/sintese/GerarCodigoIntermediario.java
+++ src/sintese/GerarCodigoIntermediario.java
@@ -21,9 +21,18 @@ public class GerarCodigoIntermediario {
     }
     
     public void Gerar(){
+        Registrador r = new Registrador();
+        String salvar = Construir(tabela, r);
+        geraArquivo(salvar);
+    }
+    
+    private String Construir(ArrayList<Tokens> tabela, Registrador r){
         int i, j;
-        ArvoreTresEnderecos t = new ArvoreTresEnderecos();
+        Calculos cal = new Calculos();
+        Condicionais con = new Condicionais();
         ArrayList<String> linha;
+        ArrayList<String> aux;
+        ArrayList<Tokens> bloco;
         ArrayList<String> cod;
         String salvar = "";
         
@@ -37,12 +46,39 @@ public class GerarCodigoIntermediario {
                 for(; i < tabela.size() && !tabela.get(i).getToken().equals("tk_fim_lin"); i++)
                     linha.add(tabela.get(i).getCadeia());
                 
-                cod = t.Excutar(linha, tipo);
+                cal.setR(r);
+                cod = cal.ExcutarCalculo(linha, tipo);
+                r = cal.getR();
+                
+                for(j = 0; j < cod.size(); j++)
+                    salvar += cod.get(j)+'\n';
+            }else
+            if(tabela.get(i).getToken().equals("tk_bloco_if")){
+                linha = new ArrayList();
+                bloco = new ArrayList();
+                
+                i+=2;//pula para os dados
+                for(; i < tabela.size() && !tabela.get(i).getToken().equals("tk_fecha_par"); i++){
+                    linha.add(tabela.get(i).getCadeia());
+                }
+                i++;//pula {
+                for(; i < tabela.size() && !tabela.get(i).getToken().equals("tk_fecha_chav"); i++){
+                    bloco.add(new Tokens(tabela.get(i).getCadeia(),tabela.get(i).getToken() , 0, 0));
+                }                
+                
+                /*Bloco*/
+                aux = Converter(Construir(bloco, r));
+                
+                con.setR(r);
+                cod = con.ExcutarIf(linha, aux);
+                r = con.getR();
+                
                 for(j = 0; j < cod.size(); j++)
                     salvar += cod.get(j)+'\n';
             }
         }
-        geraArquivo(salvar);
+        
+        return salvar;
     }
     
     private void geraArquivo(String codigo) {
@@ -58,4 +94,14 @@ public class GerarCodigoIntermediario {
             Logger.getLogger(GerarCodigoIntermediario.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+    
+    private ArrayList<String> Converter(String str){
+        ArrayList<String> aux = new ArrayList();
+      
+        String[] items = str.split("\n");
+        for (String item : items) 
+           aux.add(item);
+        
+        return aux;
+    }    
 }
